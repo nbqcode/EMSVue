39fe6880e5fa85a9e162771689eba8f3fa09ea4b
nbqcode <126157699+nbqcode@users.noreply.github.com>
2023-12-28 00:04:11 +0800

    delete docs

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import "reset-css";
+import "@/assets/css/main.scss";
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.mount("#app");
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,6 @@
     <h1>This is an about page</h1>
   </div>
 </template>
-
 <style>
 @media (min-width: 1024px) {
   .about {
